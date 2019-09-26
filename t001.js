d4d1bb3e53136763bed506b8f7a88aabba2ffe38
catonlan <catonlan@163.com>
2019-09-26 23:42:56 +0800

    js 1

---
+++ t001.js
@@ -0,0 +1,25 @@
+
+// js 
+
+
+/* boolean 逻辑判断-两种：true 和 false*/
+
+var bool = true;
+console.log(typeof bool); //返回 boolean
+
+
+
+
+/* null 为空的对象*/ 
+
+var a = null;
+console.log(a);
+console.log(typeof a); // 返回 object
+
+
+
+/*undefind  未定义：只声明变量 没给赋值*/
+
+var b
+console.log(b);
+console.log(typeof b); // 返回 undefined
