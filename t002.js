a51d6949fc213b93b0a86be3af30b7f42eefe155
catonlan <catonlan@163.com>
2019-09-26 23:59:46 +0800

    t002.js

---
+++ t002.js
@@ -0,0 +1,43 @@
+// js document
+
+
+/* 转换 string --------------------------------------------------------------- */
+
+//method 1  调用被转换数据类型的 toString()方法
+            //null 和 undefined 转不了会报错
+
+
+var a =123;
+
+console.log(a);
+console.log(typeof a); //data type:number 123
+
+
+var a = a.toString();
+console.log(a);
+console.log(typeof a); //data type:string 123
+
+
+var b = true;
+console.log(b);
+console.log(typeof b); //data type: boolean
+
+
+var b = b.toString();
+console.log(b);
+console.log(typeof b); //data type: string 
+
+
+
+
+//method 2  调用String()函数，并将其转换的数据作为参数传递给函数
+            // null 和 undefined 可转成"null" "undefined"
+
+
+var c = 123;
+    c = String(c);
+                       
+console.log(c);         
+console.log(typeof c); // 123 string
+
+
