8f8e1d15ff7122fd2a69529ff813e5222461c7aa
catonlan <catonlan@163.com>
2019-10-21 20:53:40 +0800

    变量 和类型

--- README.md
+++ README.md
@@ -1,2 +1,6 @@
 # basejs-learn
 javascript 基础练习
+
+# t038.js
+后面的是javascript 权威指南 的代码
+
---
+++ q001.js
@@ -0,0 +1,16 @@
+// *JS
+
+var x; // 声明一个变量x
+
+x = 0; // 变量x值为0;
+
+// javascript 支持多种数据类型
+
+x = 1; // 数字
+x = 0.01; // 整数和实数公用的一种类型
+x = 'hello world'; // 双引号字符串
+x = 'javascript'; // 单引号字符串
+x = true; //  布尔值
+x = false; // 另一个布尔值;
+x = null; // null 是一个特殊的值, 意思是空;
+x = undefined; // undefined 和 null 非常类似  ->未定义
